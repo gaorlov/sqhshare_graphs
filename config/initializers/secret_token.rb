70e33f6b54906552424c920260de18c5d58c2dab
grabby <grabby@grabby-beast.(none)>
2013-05-11 12:16:41 -0700

    initial commit: rails app, with some gemfile modifications

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SqlshareGraphs::Application.config.secret_token = '9ef8b8793fa934111c2f498c8243c1b574a6083ea2e6d1ee8364f1d525a03960febfbd2f5ac4546e805116fb0c78a6925b371d04ab41bf9c25f50c384d464e05'
